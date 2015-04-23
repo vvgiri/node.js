05af503c00be34a7fb9d84b0f1c206d7dec073e7
Vijay Giri <vgiri@walmart.com>
2015-04-23 16:15:57 -0700

    codepath node bootcamp prework

---
+++ index.js
@@ -0,0 +1,44 @@
+let http = require('http')
+let fs = require('fs')
+let request = require('request')
+let through = require('through')
+let argv = require('yargs')
+	.default('host', '127.0.0.1')
+	.argv
+let scheme = 'http://'
+let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
+let destinationUrl = argv.url || scheme + argv.host + ':' + port
+let logStream = argv.logFile ? fs.createWriteStream(argv.logFile)
+: process.stdout
+
+http.createServer((req, res) => {
+	logStream.write('\nEcho request: \n' + JSON.stringify(req.headers))
+	for ( let header in req.headers) {
+		res.setHeader(header, req.headers[header])
+	}
+	through(req, logStream, {autoDestroy: false})
+	req.pipe(res)
+}).listen(8000)
+
+logStream.write('Listening at http://127.0.0.1:8000')
+
+http.createServer((req, res) => {
+	let url = destinationUrl
+	if (req.headers['x-destination-url']) {
+		url = req.headers['x-destination-url']
+	}
+	let options = {
+		headers: req.headers,
+		url: url + req.url
+	}
+
+	logStream.write('\nProxy request: \n' + JSON.stringify(req.headers))
+	through(req, logStream, {autoDestroy: false})
+
+	let destinationResponse = req.pipe(request(options))
+
+	logStream.write(JSON.stringify(destinationResponse.headers))
+	destinationResponse.pipe(res)
+	through(destinationResponse, logStream, {autoDestroy: false})
+}).listen(8001)
+
